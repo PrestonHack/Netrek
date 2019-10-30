303a95f598dc50cf172ab1c19b6dc319b84225a5
Preston1337 <preston.hackett@okstate.edu>
2019-10-30 01:27:02 -0500

    Start of fuel manager didn't get very far but I found the right numbers for the ticks for recharge and costs

---
+++ Assets/FuelController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelController : MonoBehaviour
+{
+    [SerializeField]
+    private float tickTimer;
+    [SerializeField]
+    private float nextTime;
+    public float maxFuel;
+    public float currentFuel;
+    public float totalFuelCost;
+    [SerializeField]
+    private int rechargeRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tickTimer = 0.1f;
+        maxFuel = 10000;
+        currentFuel = 8000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {               
+        if (Time.time > nextTime )
+        {
+            nextTime = Time.time + tickTimer;
+            currentFuel += rechargeRate;
+
+            currentFuel = Mathf.Clamp(currentFuel, 0, maxFuel);
+            Debug.Log(currentFuel.ToString());
+
+        }
+        
+    }
+
+}
