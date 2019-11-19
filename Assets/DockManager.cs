ff04f6961e711605df649573765a30922f656faf
Preston1337 <preston.hackett@okstate.edu>
2019-11-19 17:59:05 -0600

    started on the docking system got the boiler plate for it down just need to finish it off.

---
+++ Assets/DockManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DockManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] docks;
+    [SerializeField]
+    private CircleCollider2D dockCollider;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.transform.root.name + " is inside the docking collider: DockManager");
+    }
+}
